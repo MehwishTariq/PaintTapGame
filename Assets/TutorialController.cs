024f30810a26c78d0e9033ed9d3a8c51701fa7ed
Mehwish Tariq <mta.34091@gmail.com>
2024-01-03 23:22:45 +0500

    Added confetti

---
+++ Assets/TutorialController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    public static Action cameraRot;
+    public static Action cameraZoom;
+    public static Action colorSelect;
+    public static Action tapOnObj;
+
+    public GameObject arrowHand;
+    public List<Animation> handAnim;
+
+
+
+}
